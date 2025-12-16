216b958c71ffcd14749160ddeea4b7a51dc6a85f
ganingcnl <55273059+ganingcnl@users.noreply.github.com>
2025-12-17 00:57:51 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ const tombol = document.getElementById("tombolKlik");
 const pesan = document.getElementById("pesan");
 
 tombol.addEventListener("click", () => {
-    pesan.textContent = "✨ Tombol berhasil diklik! Website kamu sudah online 🚀";
+    pesan.innerText = "Tombol berhasil diklik!";
 });
